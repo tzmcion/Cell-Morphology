db8e48979a25fb0e33cf9d20c2b143c1d790c6fe
tzmcion <tymsonekjelonek@gmail.com>
2024-10-09 17:49:29 +0200

    finished brightness operator

--- App/Components/Transformations/Transformations.h
+++ App/Components/Transformations/Transformations.h
@@ -74,8 +74,10 @@ class Transformations{
 
     /**
      *  Function alters brightnes on a circle
+     *  TODO: Calculatio of mean depends on the distance from the center
+     *  Maybe thanks to this it will work in some other way idk
      * */
-    static void alter_mean_of_circle(cv::Mat &img, cv::Point center, int goal_brightness, int radius=50){
+    static void alter_mean_of_circle(cv::Mat &img, cv::Point center, int goal_brightness, int radius=50, int threshold_val = 20){
         //First, calculate boundries
         const int rows=img.rows, cols=img.cols;
         const int x_max = std::min(rows,center.x + radius);
@@ -88,25 +90,32 @@ class Transformations{
         int count = 0;
         double sum = 0;
         std::vector<cv::Point> points;
+        std::vector<double> influence;
         for(int x = x_min; x < x_max; x++){
             for(int y = y_min; y < y_max; y++){
                 const int distance = std::floor(std::sqrt(std::pow(x - center.x,2) + std::pow(y - center.y,2)));
                 if(distance <= radius){
-                    sum += img.at<cv::uint8_t>(x,y);
+                    if(abs(goal_brightness - img.at<float>(x,y)) > threshold_val){
+                        cv::Point ap_point(x,y);
+                        influence.push_back(double(radius - distance) / radius);
+                        points.push_back(ap_point);
+                        continue;
+                    } 
+                    sum += img.at<float>(x,y);
                     count += 1;
                     cv::Point ap_point(x,y);
+                    influence.push_back(double(radius - distance) / radius);
                     points.push_back(ap_point);
                 }
             }
         }
         double ajd_mean = goal_brightness - sum/count;
-        //Now, the loop has to be done again to alter the points depending on brightnes
-        //Let's also add statament that alter cannot be bigger than 5
-        if(abs(ajd_mean) > 4){ajd_mean = 4 * (ajd_mean > 0 ? 1 : -1);}
+        //Influence size must be same size as points
         for(int i = 0; i < points.size(); i++){
             const int x = points[i].x;
             const int y = points[i].y;
-            img.at<cv::uint8_t>(x,y) = cv::saturate_cast<cv::uint8_t>(img.at<cv::uint8_t>(x,y) + ajd_mean);
+            img.at<float>(x,y) += ajd_mean*influence[i];
+            //std::cout<< ajd_mean*influence[i] << std::endl;
         }
         //Should work :)
     }
@@ -134,8 +143,10 @@ class Transformations{
 
     //TODO:
     //Decide if algorithm will go 8 ways or 4 ways;
-    static void dijkstra_mean_alter(cv::Mat &img,cv::Point start_point, int brightnes,int radius = 50, int alter_radius = 100){
+    static void dijkstra_mean_alter(cv::Mat &img_o,cv::Point start_point, int brightnes,int radius = 50, int alter_radius = 100){
         Entites::Queue<Transformations::dijkstra_point> queue;
+        cv::Mat img;
+        img_o.convertTo(img, CV_32F);
         //vectors collecting already visited points
         std::vector<cv::Point> visited_right;
         std::vector<cv::Point> visited_left;
@@ -152,11 +163,14 @@ class Transformations{
         queue.append(dijkstra_point(cv::Point(center.x - radius, center.y),&visited_left));
         queue.append(dijkstra_point(cv::Point(center.x, center.y - radius),&visited_bottom));
 
+        alter_mean_of_circle(img,center,brightnes,alter_radius);
 
         while(!queue.empty()){
             const dijkstra_point p = queue.pop();
             visited = p.visited;
             center = p.center;
+            // cv::imshow("Changes",img);
+            // cv::waitKey(0);
             if(center.x < 0 || center.x > img.rows || center.y < 0 || center.y > img.cols){
                 //If center is out of bonds, continue
                 //TODO:
@@ -167,13 +181,14 @@ class Transformations{
                 //Here check if this point was already visited by this instance
                 continue;
             }
-            alter_mean_of_circle(img,center,brightnes,alter_radius);
+            alter_mean_of_circle(img,center,brightnes,alter_radius,20);
             visited->push_back(cv::Point(center));
             queue.append(dijkstra_point(cv::Point(center.x + radius, center.y),visited));
             queue.append(dijkstra_point(cv::Point(center.x, center.y + radius),visited));
             queue.append(dijkstra_point(cv::Point(center.x - radius, center.y),visited));
             queue.append(dijkstra_point(cv::Point(center.x, center.y - radius),visited));
         }
+        img.convertTo(img_o,CV_8U);
     }
 };
 
