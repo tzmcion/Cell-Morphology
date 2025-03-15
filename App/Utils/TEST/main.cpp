31578fb1129f9fb65de7e9e0124deb053c77c606
tzmcion <tymsonekjelonek@gmail.com>
2025-03-15 22:41:56 +0100

    I go watch TV will continue tomoroow

---
+++ App/Utils/TEST/main.cpp
@@ -0,0 +1,17 @@
+#include "../../Components/Watershed/Watershed.h"
+#include "../../Components/Structures/Colors.h"
+#include "../../Components/Transformations/Transformations.h"
+#include "../../Components/Structures/Structures.h"
+#include "../../Components/Optimalization/Optimaliation.h"
+#include "../../Components/Threading/Threads.h"
+
+/**
+    OH BROTHER NOW I HAE TO WRITE UNIT TESTS UH
+*/
+
+int main(int argc, char **argv){
+
+    
+    
+    return 0;
+}
